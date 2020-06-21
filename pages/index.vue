c03496287187ef750b8b9d6042cae8f578c9499a
brenetic <b@brenetic.com>
2020-06-21 15:22:52 +0100

    Hide the H1 div

--- pages/index.vue
+++ pages/index.vue
@@ -44,9 +44,9 @@ export default {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
+  visibility: hidden;
 }
 
 .subtitle {
